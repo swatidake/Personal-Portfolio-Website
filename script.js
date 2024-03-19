26fcd58fb4efb59b706b7c346bf3cda78c94f576
Swati Dake <131987340+swatidake@users.noreply.github.com>
2024-03-19 14:03:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+ 
+// For Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+ //Display massage
+    function display(){
+        alert("Submitted Successfully...!")
+    }
+ 
